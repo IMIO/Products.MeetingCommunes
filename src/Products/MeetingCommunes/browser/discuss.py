105908bead4dcd345f3dfd9bf2c682e8f168cc62
Gauthier Bastien <g.bastien@imio.be>
2013-06-19 12:12:38 +0000

    Do not override the discuss.available to set it to True as it is True now by default in PloneMeeging...

--- src/Products/MeetingCommunes/browser/discuss.py
+++
@@ -1,8 +0,0 @@
-from Products.PloneMeeting.browser.async import Discuss
-
-
-class CustomDiscuss(Discuss):
-
-    #just override the Discuss.available method
-    def available(self):
-        return True
