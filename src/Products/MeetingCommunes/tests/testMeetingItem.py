347ad9c7ce0e9ce62ee195ebf84bec013593133d
Gauthier Bastien <g.bastien@imio.be>
2013-10-09 12:07:36 +0000

    Call test_pm_manageItemAssembly test from PloneMeeting

--- src/Products/MeetingCommunes/tests/testMeetingItem.py
+++ src/Products/MeetingCommunes/tests/testMeetingItem.py
@@ -114,6 +114,10 @@ class testMeetingItem(MeetingCommunesTestCase, pmtmi):
         '''See doc string in PloneMeeting.'''
         self.test_pm_IsLateFor()
 
+    def test_subproduct_call_manageItemAssembly(self):
+        '''See doc string in PloneMeeting.'''
+        self.test_pm_manageItemAssembly()
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
