c90003ecc5879b2785d5f81b6e4d6fdb7c6ea3aa
Gauthier Bastien <g.bastien@imio.be>
2013-10-22 12:28:35 +0000

    Called new tests added to PloneMeeting regarding ticket #6796

--- src/Products/MeetingCommunes/tests/testMeeting.py
+++ src/Products/MeetingCommunes/tests/testMeeting.py
@@ -33,41 +33,73 @@ class testMeeting(MeetingCommunesTestCase, pmtm):
     """
 
     def test_subproduct_call_InsertItem(self):
-        """Run the test_pm_InsertItem from PloneMeeting."""
+        """Run the test_pm_InsertItem from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_InsertItem()
 
+    def test_subproduct_call_InsertItemOnProposingGroupsWithDisabledGroup(self):
+        """Run the test_pm_InsertItemOnProposingGroupsWithDisabledGroup from PloneMeeting.
+           See docstring in PloneMeeting."""
+        self.test_pm_InsertItemOnProposingGroupsWithDisabledGroup()
+
     def test_subproduct_call_InsertItemCategories(self):
-        """Run the test_pm_InsertItemCategories from PloneMeeting."""
+        """Run the test_pm_InsertItemCategories from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_InsertItemCategories()
 
+    def test_subproduct_call_InsertItemOnCategoriesWithDisabledCategory(self):
+        """Run the test_pm_InsertItemOnCategoriesWithDisabledCategory from PloneMeeting.
+           See docstring in PloneMeeting."""
+        self.test_pm_InsertItemOnCategoriesWithDisabledCategory()
+
     def test_subproduct_call_InsertItemAllGroups(self):
-        """Run the test_pm_InsertItemAllGroups from PloneMeeting."""
+        """Run the test_pm_InsertItemAllGroups from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_InsertItemAllGroups()
 
+    def test_subproduct_call_InsertItemOnAllGroupsWithDisabledGroup(self):
+        """Run the test_pm_InsertItemOnAllGroupsWithDisabledGroup from PloneMeeting.
+           See docstring in PloneMeeting."""
+        self.test_pm_InsertItemOnAllGroupsWithDisabledGroup()
+
     def test_subproduct_call_InsertItemPrivacyThenProposingGroups(self):
-        """Run the test_pm_InsertItemPrivacyThenProposingGroups from PloneMeeting."""
+        """Run the test_pm_InsertItemPrivacyThenProposingGroups from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_InsertItemPrivacyThenProposingGroups()
 
+    def test_subproduct_call_InsertItemPrivacyThenProposingGroupsWithDisabledGroup(self):
+        """Run the test_pm_InsertItemPrivacyThenProposingGroupsWithDisabledGroup from PloneMeeting.
+           See docstring in PloneMeeting."""
+        self.test_pm_InsertItemPrivacyThenProposingGroupsWithDisabledGroup()
+
     def test_subproduct_call_InsertItemPrivacyThenCategories(self):
-        """Run the test_pm_InsertItemPrivacyThenCategories from PloneMeeting."""
+        """Run the test_pm_InsertItemPrivacyThenCategories from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_InsertItemPrivacyThenCategories()
 
+    def test_subproduct_call_InsertItemPrivacyThenCategoriesWithDisabledCategory(self):
+        """Run the test_pm_InsertItemPrivacyThenCategoriesWithDisabledCategory from PloneMeeting.
+           See docstring in PloneMeeting."""
+        self.test_pm_InsertItemPrivacyThenCategoriesWithDisabledCategory()
+
     def test_subproduct_call_RemoveOrDeleteLinkedItem(self):
-        """Run the test_pm_RemoveOrDeleteLinkedItem from PloneMeeting."""
+        """Run the test_pm_RemoveOrDeleteLinkedItem from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_RemoveOrDeleteLinkedItem()
 
     def test_subproduct_call_MeetingNumbers(self):
-        """Run the test_pm_MeetingNumbers from PloneMeeting."""
+        """Run the test_pm_MeetingNumbers from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_MeetingNumbers()
 
     def test_subproduct_call_AvailableItems(self):
-        """Run the testAvailableItems from PloneMeeting."""
+        """Run the test_pm_AvailableItems from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_AvailableItems()
 
     def test_subproduct_call_PresentSeveralItems(self):
-        """
-          Run the testPresentSeveralItems from PloneMeeting
-        """
+        """Run the testPresentSeveralItems from PloneMeeting.
+           See docstring in PloneMeeting."""
         self.test_pm_PresentSeveralItems()
 
     def test_subproduct_call_DecideSeveralItems(self):
