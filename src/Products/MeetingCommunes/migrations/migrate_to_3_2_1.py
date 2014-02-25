8ab5ef2fe6e594c8a19ede96218d1bf6a9c1743d
Gauthier Bastien <g.bastien@imio.be>
2014-02-25 14:42:14 +0000

    Added migration to 3.2.1 and removed migrations before 3.2.0 as this version is not backward compatible with 3.1.0 and under

--- src/Products/MeetingCommunes/migrations/migrate_to_3_2_1.py
+++ src/Products/MeetingCommunes/migrations/migrate_to_3_2_1.py
@@ -1,16 +1,17 @@
-# ------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+
 import logging
 logger = logging.getLogger('PloneMeeting')
+
 from Products.PloneMeeting.migrations import Migrator
 
 
 # The migration class ----------------------------------------------------------
-class Migrate_To_3_1_0(Migrator):
+class Migrate_To_3_2_1(Migrator):
 
     def run(self):
-        logger.info('Migrating to MeetingCommunes 3.1.0...')
-        # reinstall so update in meetingitemcouncil_workflow regarding backToCreated
-        # transition renamed to backToItemCreated is applied
+        logger.info('Migrating to MeetingCommunes 3.2.1...')
+        # reinstall so skins and so on are correct
         self.reinstall(profiles=[u'profile-Products.MeetingCommunes:default', ])
         self.finish()
 
@@ -19,7 +20,7 @@ class Migrate_To_3_1_0(Migrator):
 def migrate(context):
     '''This migration function:
 
-       1) Reinstall MeetingCommunes so workflow are updated (meetingitemcouncil_workflow);
+       1) Reinstall Products.MeetingCommunes so skin and so on are correct.
     '''
-    Migrate_To_3_1_0(context).run()
+    Migrate_To_3_2_1(context).run()
 # ------------------------------------------------------------------------------
