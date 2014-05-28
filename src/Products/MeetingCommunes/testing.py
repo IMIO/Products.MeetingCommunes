72942509a22c0bbea6ca430d569c9b0c0f06d802
Vivian Antoine <vivian.antoine@imio.be>
2014-05-28 14:36:41 +0000

    add layer for robot testing

--- src/Products/MeetingCommunes/testing.py
+++ src/Products/MeetingCommunes/testing.py
@@ -3,7 +3,7 @@ from plone.testing import z2, zca
 from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import FunctionalTesting
 import Products.MeetingCommunes
-
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 
 MC_ZCML = zca.ZCMLSandbox(filename="testing.zcml",
                           package=Products.MeetingCommunes,
@@ -23,3 +23,21 @@ MC_TESTING_PROFILE = PloneWithPackageLayer(
 
 MC_TESTING_PROFILE_FUNCTIONAL = FunctionalTesting(
     bases=(MC_TESTING_PROFILE,), name="MC_TESTING_PROFILE_FUNCTIONAL")
+
+MC_EXAMPLES_FR_TESTING_PROFILE = PloneWithPackageLayer(
+    zcml_filename="testing.zcml",
+    zcml_package=Products.MeetingCommunes,
+    additional_z2_products=('Products.MeetingCommunes',
+                            'Products.PloneMeeting',
+                            'Products.CMFPlacefulWorkflow'),
+    gs_profile_id='Products.MeetingCommunes:examples_fr',
+    name="MC_TESTING_PROFILE")
+
+MC_TESTING_ROBOT = FunctionalTesting(
+    bases=(
+        MC_EXAMPLES_FR_TESTING_PROFILE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name="MC_TESTING_ROBOT",
+)
