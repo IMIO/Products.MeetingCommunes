284f1310534fac216eb69860a19f57e515b3b3e2
Gauthier Bastien <g.bastien@imio.be>
2014-11-19 13:11:39 +0000

    Added new test override from PloneMeeting

--- src/Products/MeetingCommunes/tests/testMeetingItem.py
+++ src/Products/MeetingCommunes/tests/testMeetingItem.py
@@ -199,6 +199,10 @@ class testMeetingItem(MeetingCommunesTestCase, pmtmi):
         '''See doc string in PloneMeeting.'''
         self.test_pm_TakenOverBy()
 
+    def test_subproduct_call_ItemActionsPanelCaching(self):
+        '''See doc string in PloneMeeting.'''
+        self.test_pm_ItemActionsPanelCaching()
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
