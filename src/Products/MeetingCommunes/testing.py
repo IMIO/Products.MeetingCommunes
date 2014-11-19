120692a2a8d6d00a06c916a00bccc3e3c4dc12aa
Gauthier Bastien <g.bastien@imio.be>
2014-11-19 19:39:51 +0000

    Use PloneMeetingLayer where overrides.zcml is loaded

--- src/Products/MeetingCommunes/testing.py
+++ src/Products/MeetingCommunes/testing.py
@@ -2,6 +2,7 @@
 from plone.testing import z2, zca
 from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import FunctionalTesting
+from Products.PloneMeeting.testing import PloneMeetingLayer
 import Products.MeetingCommunes
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 
@@ -12,7 +13,7 @@ MC_ZCML = zca.ZCMLSandbox(filename="testing.zcml",
 MC_Z2 = z2.IntegrationTesting(bases=(z2.STARTUP, MC_ZCML),
                               name='MC_Z2')
 
-MC_TESTING_PROFILE = PloneWithPackageLayer(
+MC_TESTING_PROFILE = PloneMeetingLayer(
     zcml_filename="testing.zcml",
     zcml_package=Products.MeetingCommunes,
     additional_z2_products=('Products.MeetingCommunes',
