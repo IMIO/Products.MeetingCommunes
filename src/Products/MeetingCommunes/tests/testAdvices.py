6c1967ae315b2a404dd6cd3e199a699a570b79f4
Gauthier Bastien <g.bastien@imio.be>
2015-07-08 11:09:31 +0000

    Call new test from PM

--- src/Products/MeetingCommunes/tests/testAdvices.py
+++ src/Products/MeetingCommunes/tests/testAdvices.py
@@ -130,6 +130,10 @@ class testAdvices(MeetingCommunesTestCase, pmta):
         '''Run the test_pm_ChangeAdviceHiddenDuringRedactionView from PloneMeeting.'''
         pmta.test_pm_ChangeAdviceHiddenDuringRedactionView(self)
 
+    def test_subproduct_call_ChangeAdviceAskedAgainView(self):
+        '''Run the test_pm_ChangeAdviceHiddenDuringRedactionView from PloneMeeting.'''
+        pmta.test_pm_ChangeAdviceAskedAgainView(self)
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
