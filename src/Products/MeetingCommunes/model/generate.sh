f564434251414c22ca36d002230824a6fb699cb5
Gauthier Bastien <g.bastien@imio.be>
2016-05-03 08:20:58 +0000

    Moved financesadvice specific stuff (wf, types) to a separated profile

--- src/Products/MeetingCommunes/model/generate.sh
+++ src/Products/MeetingCommunes/model/generate.sh
@@ -2,5 +2,7 @@
 /srv/archgenxml/bin/archgenxml --cfg generate.conf MeetingCommunes.zargo -o tmp
 
 # only keep workflows
-cp -rf tmp/profiles/default/workflows ../profiles/default
+cp -rf tmp/profiles/default/workflows/meetingcommunes_workflow ../profiles/default/workflows
+cp -rf tmp/profiles/default/workflows/meetingitemcommunes_workflow ../profiles/default/workflows
+cp -rf tmp/profiles/default/workflows/meetingadvicefinances_workflow ../profiles/financesadvice/workflows
 rm -rf tmp
