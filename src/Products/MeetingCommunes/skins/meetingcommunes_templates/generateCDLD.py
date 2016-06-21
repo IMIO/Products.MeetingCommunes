89c633728f5f12dc9f81182c80e9d13cd0087cfa
Vivian Antoine <vivian.antoine@imio.be>
2016-06-21 08:36:10 +0000

    Do not hardcode the meetingconfig for finance advice synthesis anymore.

--- src/Products/MeetingCommunes/skins/meetingcommunes_templates/generateCDLD.py
+++ src/Products/MeetingCommunes/skins/meetingcommunes_templates/generateCDLD.py
@@ -10,7 +10,9 @@
 
 context.REQUEST.set('templateId', 'generate-cdld')
 # this is a dummy item we use because generateDocument here above need an item object...
-brains=context.portal_catalog.searchResults(portal_type='MeetingItemCollege', sort_limit=1)
+tool = context.portal_plonemeeting
+cfg = tool.getMeetingConfig(context)
+brains=context.portal_catalog.searchResults(portal_type=cfg.getItemTypeName(), sort_limit=1)
 context.REQUEST.set('objectUid', brains[0].UID)
 
 return context.portal_plonemeeting.generateDocument()
