6b49904fc1d8a97142ee0b422e7f0fa27f031212
Gauthier Bastien <g.bastien@imio.be>
2017-02-14 17:03:00 +0000

    predefined_title must be unicode

--- src/Products/MeetingCommunes/profiles/testing/import_data.py
+++ src/Products/MeetingCommunes/profiles/testing/import_data.py
@@ -35,7 +35,7 @@ financialAnalysisSubAnnex = ItemAnnexSubTypeDescriptor(
 
 financialAnalysis = ItemAnnexTypeDescriptor(
     'financial-analysis', 'Financial analysis', u'financialAnalysis.png',
-    'Predefined title for financial analysis', subTypes=[financialAnalysisSubAnnex])
+    u'Predefined title for financial analysis', subTypes=[financialAnalysisSubAnnex])
 
 legalAnalysis = ItemAnnexTypeDescriptor(
     'legal-analysis', 'Legal analysis', u'legalAnalysis.png')
