a48175101c8c72cc6251af5004ba73ef4d5d332b
Gauthier Bastien <g.bastien@imio.be>
2020-11-16 19:05:35 +0100

    Enable testVotes in testmc

--- src/Products/MeetingCommunes/tests/MeetingCommunesTestCase.py
+++ src/Products/MeetingCommunes/tests/MeetingCommunesTestCase.py
@@ -34,7 +34,7 @@ class MeetingCommunesTestCase(PloneMeetingTestCase, MeetingCommunesTestingHelper
     # by default, PloneMeeting's test file testPerformances.py and
     # testConversionWithDocumentViewer.py' are ignored, override the subproductIgnoredTestFiles
     # attribute to take these files into account
-    subproductIgnoredTestFiles = ['test_robot.py', 'testPerformances.py', 'testContacts.py', 'testVotes.py']
+    subproductIgnoredTestFiles = ['test_robot.py', 'testPerformances.py', 'testContacts.py']
 
     layer = MC_TESTING_PROFILE_FUNCTIONAL
 
