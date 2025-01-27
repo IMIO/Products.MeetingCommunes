1b72b38ef070ae3707091f688a9d68cc7a3e6d02
Gauthier Bastien <gauthier.bastien@imio.be>
2025-01-27 16:46:13 +0100

    Use translated values of state/transition title of MeetingConfig.itemWFValidationLevels so we check that it does not break tests when using special chars

--- src/Products/MeetingCommunes/profiles/testing/import_data.py
+++ src/Products/MeetingCommunes/profiles/testing/import_data.py
@@ -2,11 +2,12 @@
 
 from copy import deepcopy
 from Products.PloneMeeting.profiles.testing import import_data as pm_import_data
+from zope.globalrequest import getRequest
+from zope.i18n import translate
 
 
 # Meeting configurations -------------------------------------------------------
-# college
-
+# Collège
 collegeMeeting = deepcopy(pm_import_data.meetingPma)
 collegeMeeting.id = 'meeting-config-college'
 collegeMeeting.title = 'Collège Communal'
@@ -20,6 +21,27 @@ collegeMeeting.itemActionsInterface = 'Products.MeetingCommunes.interfaces.IMeet
 collegeMeeting.meetingConditionsInterface = 'Products.MeetingCommunes.interfaces.IMeetingCommunesWorkflowConditions'
 collegeMeeting.meetingActionsInterface = 'Products.MeetingCommunes.interfaces.IMeetingCommunesWorkflowActions'
 collegeMeeting.workflowAdaptations = ['no_publication', 'pre_accepted', 'accepted_but_modified', 'delayed', 'refused']
+# translate state_title and transition_title of every itemWFValidationLevels
+item_wf_val_levels = collegeMeeting.itemWFValidationLevels
+request = getRequest()
+for level in item_wf_val_levels:
+    level['state_title'] = translate(
+        level['state_title'],
+        domain="plone",
+        context=request,
+        target_language="fr").encode('utf-8')
+    level['leading_transition_title'] = translate(
+        level['leading_transition_title'],
+        domain="plone",
+        context=request,
+        target_language="fr").encode('utf-8')
+    level['back_transition_title'] = translate(
+        level['back_transition_title'],
+        domain="plone",
+        context=request,
+        target_language="fr").encode('utf-8')
+collegeMeeting.itemWFValidationLevels = item_wf_val_levels
+
 # Conseil communal
 councilMeeting = deepcopy(pm_import_data.meetingPga)
 councilMeeting.id = 'meeting-config-council'
